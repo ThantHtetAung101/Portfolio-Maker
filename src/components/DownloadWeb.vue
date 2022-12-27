4777682e5db7e8592d8be844664b741c06aa8fa7
Thant Htet Aung <114596700+ThantHtetAung101@users.noreply.github.com>
2022-12-27 16:25:29 +0630

    Fixing Bugs

--- src/components/DownloadWeb.vue
+++ src/components/DownloadWeb.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="vh-100" :style="{ backgroundColor: color }">
+    <div class="h-100" :style="{ backgroundColor: color }">
         <component :color="color" v-for="(component, index) in components" :key="index" :component="component"
             v-bind:is="component.componentName">
         </component>
